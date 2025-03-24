71feec82e6af8d5ff95ec519e8f7c0d240ceaf56
seogideogi <rhodalee78@gmail.com>
2025-03-24 17:32:03 +0900

    GAT 커뮤니티 탐지 피쳐 추가

--- real_project/train10.py
+++ real_project/train10.py
@@ -3,7 +3,7 @@ import torch.nn.functional as F
 from torch_geometric.loader import DataLoader
 from sklearn.metrics import f1_score, precision_score, roc_auc_score
 from model11 import GATImproved
-from dataset10 import AMLtoGraph
+from dataset11 import AMLtoGraph
 import numpy as np
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
